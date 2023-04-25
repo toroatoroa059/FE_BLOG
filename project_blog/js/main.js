bf642603d08fea06c050ca97cfdee73487358795
toroatoroa059 <truong1155.gl@gmail.com>
2023-04-26 01:27:51 +0700

    landing-page project

--- project_blog/js/main.js
+++ project_blog/js/main.js
@@ -1,11 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-menu");
 $(document).ready(function(){
 	$(window).scroll(function(){
 		if($(this).scrollTop()){
 			$('header').addClass('sticky');
 		}
 	})
-	});
+	});	
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	nav.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() => {
+	hamburger.classList.remove("active");
+	nav.classList.remove("active");
+}));
 
-// $(document).ready(function(){
-// 	alert("ok");
-// })
